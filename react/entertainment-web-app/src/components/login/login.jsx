b0084df0972e34e1df4183e135e6dd71d19a4f90
aydinhafiz <aydinhafiz648@gmail.com>
2023-04-13 16:13:53 +0200

    I add loggin and sign up components

--- react/entertainment-web-app/src/components/login/login.jsx
+++ react/entertainment-web-app/src/components/login/login.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 function Login() {
-  
   return (
     <div className="container">
       <div className="content">
@@ -16,8 +16,14 @@ function Login() {
               placeholder="Password"
             />
           </div>
+
           <div className="login-button">
-            <button className="button-for-login"> Login to your account</button>
+            <Link to="/home">
+              <button className="button-for-login">
+                {" "}
+                Login to your account
+              </button>
+            </Link>
           </div>
           <p className="dont-have-account">
             Don’t have an account? <h2 className="sign-up">Sign Up</h2>{" "}
