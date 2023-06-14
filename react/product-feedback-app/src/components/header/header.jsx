63297975fbc788c44355358aac3eafe472d45512
aydinhafiz <aydinhafiz648@gmail.com>
2023-06-14 21:52:57 +0200

    starting with product feedback project

---
+++ react/product-feedback-app/src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="left-side">
+        <img src="" alt="lamp-img" className="lamp-img" />
+        <span>6</span>
+      </div>
+    </div>
+  );
+}
+export default Header;
