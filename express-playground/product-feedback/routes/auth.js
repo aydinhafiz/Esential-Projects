e45ba966aa77079de8fa5ecc050f30920bb50200
Aydin <112811545+aydinhafiz@users.noreply.github.com>
2024-06-21 15:35:01 +0200

    Update auth.js

--- express-playground/product-feedback/routes/auth.js
+++ express-playground/product-feedback/routes/auth.js
@@ -3,6 +3,7 @@ const { register, login } = require("../controllers/authController");
 
 const router = express.Router();
 
+
 router.route("/register").post(register);
 router.route("/login").post(login);
 
